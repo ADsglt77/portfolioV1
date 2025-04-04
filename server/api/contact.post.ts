7fe8e98f9ac5097631c0844caf380b403f1efcd9
Cleboost <clement.balarot@gmail.com>
2025-04-04 22:11:11 +0200

    fix🐛: validate required fields before token verification in contact form submission

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -2,14 +2,22 @@ import axios from "axios";
 const webhookUrl = "https://discord.com/api/webhooks/1357771574345990439/1f-na4mTepmaE2Q9mBrOQrqHPHux8BCkeoVyHDsHWQa_GqE1cCXR-mRm9EdYaaDMfE4q";
 
 export default defineEventHandler(async (event) => {
-  const { name, email, message, token } = await readBody(event);
-  if (!(await verifyTurnstileToken(token, event)).success) {
-    throw createError({
-      statusCode: 403,
-      statusMessage: "Invalid token",
-      message: "Invalid token",
-    });
-  }
+	const { name, email, message, token } = await readBody(event);
+	if (!name || !email || !message || !token) {
+		throw createError({
+			statusCode: 400,
+			statusMessage: "Missing required fields",
+			message: "Missing required fields",
+		});
+	}
+
+	if (!(await verifyTurnstileToken(token, event)).success) {
+		throw createError({
+			statusCode: 403,
+			statusMessage: "Invalid token",
+			message: "Invalid token",
+		});
+	}
 	axios.post(webhookUrl, {
 		embeds: [
 			{
